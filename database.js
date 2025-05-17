c20ee240e20c288b39cf3a3f4d37a28f08092b56
Guga Barrera <gustavoivan.barrera@gmail.com>
2025-05-17 12:59:18 -0300

    Integración completa de MySQL y CRUD funcional

--- database.js
+++ database.js
@@ -1,10 +1,9 @@
-// database.js
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: '', // tu contraseña si tienes una
+  user: 'guga',
+  password: '1234',
   database: 'reserva_horas'
 });
 
